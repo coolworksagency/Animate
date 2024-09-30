38a61c37854cb04a8da33cd67394fb82fc5c5d20
SaimAbbas <156453313+coolworksagency@users.noreply.github.com>
2024-09-30 06:30:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var tl = gsap.timeline({scrollTrigger:{
+    trigger: ".two",
+    start: "0% 95%",
+    end: "70% 50%",
+    scrub: true,
+    // markers: true,
+}})
+
+tl.to("#fanta",{
+    top: "120%",
+    left: "0%",
+    rotate: "80deg"
+}, 'orange')
+tl.to("#orange-cut",{
+    top:"160%",
+    left: "23%"
+}, 'orange')
+tl.to("#orange",{
+    width: "15%",
+    top:"160%",
+    right: "10%"
+}, 'orange')
+tl.to("#leaf",{
+    top:"110%",
+    rotate: "130deg",
+    left: "70%"
+}, 'orange')
+tl.to("#leaf2",{
+    top:"110%",
+    rotate: "130deg",
+    left: "0%"
+}, 'orange')
+
+
+var tl2 = gsap.timeline({scrollTrigger:{
+    trigger: ".three",
+    start: "0% 95%",
+    end: "20% 50%",
+    scrub: true,
+    // markers: true,
+}})
+
+
+
+tl2.to("#orange-cut",{
+    width:"18%",
+    left: "42%",
+    top: "204%"
+}, 'ca')
+tl2.to("#fanta",{
+    width:"35%",
+    top: "210%",
+    left: "33%",
+    rotate: "0deg"
+}, 'ca')
+
